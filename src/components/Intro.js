e2ab8384108cf7e34076ffb59fe4f376979ed4ab
Chen Qu <quchen.canada@gmail.com>
2023-04-06 16:26:14 -0400

    update principles and aboutUs

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -13,34 +13,19 @@ export const Intro = () => {
             <div className="content">
               <h2>Introduction</h2>
               <p>
-                The purpose of this report is to provide an overview of our
-                redesign project undertaken as part of the Human-Computer
-                Interaction (HCI) course. HCI is a multidisciplinary field that
-                focuses on the design and evaluation of interactive systems to
-                ensure an optimal user experience. In today's technology-driven
-                world, creating user-centered designs has become paramount for
-                the success of any product or service. Through this project, our
-                team aimed to apply the principles and methodologies taught in
-                the HCI course to redesign an existing application, ultimately
-                enhancing its usability and user satisfaction. </p>
+              <strong>Human-Computer Interaction (HCI)</strong> is an interdisciplinary field that focuses on the design, evaluation, and implementation of interactive computing systems for human use. As technology continues to evolve at a rapid pace, it is crucial to ensure that these systems are user-friendly, efficient, and accessible to a wide range of users. This report aims to provide a comprehensive analysis of a specific interactive game, with the goal of optimizing its usability and user experience.</p>
               <p>
-                The report will present a detailed account of our project, starting with the
-                identification of a suitable application for redesign, followed
-                by an in-depth analysis of its current user experience. We will
-                discuss the methods employed for gathering user feedback and the
-                insights obtained, which informed our design decisions.
-                Additionally, we will outline the iterative design process,
-                highlighting the various stages of prototyping, testing, and
-                refining our proposed solution. Finally, the report will
-                conclude with an evaluation of the redesigned application's
-                performance, a comparison with the original version, and
-                recommendations for further improvements.
+              The report is organized into five key sections, each addressing a critical aspect of the HCI design process. The first section, the executive summary, provides an overview of the report's content and highlights the most significant findings and recommendations. The second section presents a navigational map of the system under evaluation, visually illustrating the flow of user interactions and the organization of the system's components.
               </p>
+              <p>In the third section, we detail a list of 20 design principles that have been applied in the development of the game, along with justifications for their inclusion. These principles serve as guidelines for effective interaction design and are grounded in established HCI research.</p>
+              <p>The fourth section, the final heuristic evaluation, assesses the system's compliance with widely-accepted usability heuristics, providing insight into potential areas of improvement. This evaluation will help identify usability issues and establish priorities for system refinement.</p>
+              <p>Lastly, the fifth section offers thoughtful recommendations for enhancing the system based on the findings from the heuristic evaluation. These recommendations aim to address identified issues and contribute to a more seamless, enjoyable, and efficient user experience.</p>
+              <p>By systematically examining the interactive system through these five sections, this report will not only identify areas for improvement but also provide valuable insights and recommendations for enhancing the overall usability and user experience. Ultimately, our goal is to contribute to the development of more effective, accessible, and enjoyable human-computer interactions.</p>
             </div>
           </Col>
         </Row>
       </Container>
-      
+
     </section>
   );
 };
--- src/components/Principles.js
+++ src/components/Principles.js
@@ -1,61 +1,133 @@
 import { Container, Row, Col } from "react-bootstrap";
-import TableOfContent from './TableOfContent';
+import TableOfContent from "./TableOfContent";
 
 export const Principles = () => {
   return (
     <section className="Exec">
       <Container>
         <Row className="aligh-items-center">
-          <Col md='auto'>
+          <Col md="auto">
             <TableOfContent />
           </Col>
           <Col xs={8}>
             <div className="content">
               <h2>Design Principles</h2>
-              <p>
-                The main objective of the redesign is to improve the interactive
-                interfaces and interactive gameplays of the game. The redesign
-                will develop more pages/states and improve current interfaces
-                inside or outside the game session. Specifically, the redesign
-                aims to improve the main menu page and add a new game/resume
-                page, setting page, shopping page/system, achievement page, and
-                tutorial page. The original game design relies on using the
-                mouse cursor as an input to simulate Joycon controls for
-                maneuvering the "airplane". However, this approach can be
-                counterintuitive, difficult to discover, and conflicts with the
-                user's mental model, resulting in unnecessary complexity. To
-                bridge the gap between the user's gulf of evaluation and
-                execution, the use of metaphors can be a valuable technique. By
-                providing visual cues that highlight what the user should focus
-                on and what should be hidden, the experience can become more
-                intuitive and engaging. For instance, instead of relying solely
-                on mouse controls, a plane joystick can be added as a UI element
-                to simulate the physical controls of an airplane, allowing users
-                to quickly grasp the idea of controlling the plane. Similarly,
-                when using a keyboard for input, an image of a key can guide the
-                user on which button to push to complete a specific task. By
-                drawing on familiar metaphors and previous experiences of using
-                similar controls in other games, users can learn more quickly
-                and effectively how to control the airplane in this game. Our
-                project will focus on graphical interfaces, as we will not be
-                implementing voice or haptic interfaces due to hardware
-                limitations. Our in-game UI will heavily rely on icon and
-                multimedia interfaces, designed with isomorphic and flat 2D
-                principles for simplicity and readability. Our main menu will be
-                a nested menu, as we have enough selection elements that a flat
-                menu would be overwhelming for new users. This will help users
-                quickly navigate to the element they need, especially in
-                categories such as game modes. Important buttons such as “start
-                game” should be bigger than other buttons in light of Fitt’s
-                law. For the next couple weeks, we are going to create
-                wireframes or interactive prototypes to test the functionality
-                of the new features, such as the shopping page/system or the
-                game/resume page. These prototypes can be tested with potential
-                users to identify usability issues, and we can use the feedback
-                to refine their designs and improve the user experience.
-                Original Project Link:
-                https://github.com/IdoChennn/4474---Flight-simulation-video-game
-              </p>
+              <ol>
+                <li>
+                  <p>80/20 rules</p>
+
+                  <p>
+                    The original game design is overly simplistic. As players
+                    gain proficiency, they begin to demand more features and
+                    complexity. However, adding these features directly to the
+                    initial UI can result in a cluttered and confusing
+                    interface. By concentrating on the 20% of interface elements
+                    that deliver 80% of the functionality, designers can
+                    maintain a clean and user-friendly experience. To achieve
+                    this balance, it is advisable to allocate 80% of the screen
+                    space for the main content, such as the game world or
+                    application content, while reserving the remaining 20% for
+                    essential interface elements, including menus and toolbars.
+                  </p>
+                </li>
+                <li>
+                  <p>Visibility</p>
+                  <p>With the introduction of additional features in the game, visibility has become a critical aspect of creating a user-friendly and enjoyable experience. We have ensured that text, icons, and other UI elements are legible and easy to comprehend. Furthermore, we have employed colors, shapes, and animations to emphasize essential elements, capturing the player's attention during crucial information or events. For instance, health bars may change color or flash when reaching a critical level.</p>
+                </li>
+                <li>
+                  <p>Affordances</p>
+                  <p>Affordances help users understand what actions are possible by providing visual or sensory cues that indicate the object's functionality or purpose. We designed a Home button featuring an easily recognizable house icon to convey its purpose to the user. These visual cues imply that clicking or tapping the button will navigate the user to the main menu or starting screen of the game.</p>
+                </li>
+                <li>
+                  <p>Familiarity</p>
+                  <ul>
+                    <li>
+                      <p>Utilizing familiar symbols and icons enables players to swiftly comprehend and interact with various game elements without confusion. In the original project, there was no aiming point when the aircraft was shooting. As a result, we incorporated a crosshair, allowing players to easily recognize and understand their target, since it has become a standard visual cue in numerous games.</p>
+                    </li>
+                    <li>
+                      <p>We have introduced a green medical cross to represent the player's health. This symbol is familiar to users as it is commonly associated with health and medical care in various contexts. By incorporating this easily recognizable icon, players can swiftly determine their health status during gameplay.</p>
+                    </li>
+                  </ul>
+                </li>
+                <li>
+                  <p>Feedforward</p>
+                  <p>When the user hovers their cursor over a specific upgrade, a tooltip appears, displaying a brief description of the upgrade's effects. This feedforward mechanism helps users understand the benefits of each upgrade before committing to it, enhancing the decision-making process and overall user experience.</p>
+                </li>
+                <li>
+                  <p>See-through</p>
+                  <p>In our latest project, players can easily view the number of coins they have collected. As a result, we designed the game to highlight all upgradeable weapons within their purchasing range. By making these options visually apparent, players can easily identify which upgrades are accessible to them, streamlining the decision-making process and improving the overall gaming experience.</p>
+                </li>
+                <li>
+                  <p>Constrains</p>
+                  <p>In the story mode, players are required to complete Chapter 1 before gaining access to Chapter 2, and they must finish Chapter 2 before proceeding to Chapter 3. This constraint ensures that users are not overwhelmed or overly confused by gradually introducing new content and challenges as they progress through the game. If a Chapter is not accessible, its representation can be dimmed or displayed in grayscale to signify its unavailability. Users will find that clicking on it has no effect.</p>
+                </li>
+                <li>
+                  <p>Chunking</p>
+                </li>
+                <li>
+                  <p>Mapping</p>
+                </li>
+                <li>
+                  <p>Confirmation</p>
+                  <p>In our game's shop feature, we have implemented a purchase confirmation window that displays the item the player intends to buy, along with its price and the remaining balance after the purchase is completed. This feature enables users to review their transaction details and make informed decisions before finalizing their purchases, preventing potential errors or misunderstandings.</p>
+                </li>
+                <li>
+                  <p>Progressive disclosure</p>
+                  <ul>
+                    <li>
+                      <p>Interactive tutorials: Introduce game controls and mechanics gradually as players progress through the game. This allows players to learn at their own pace and minimizes the risk of overwhelming them with too much information at once.</p>
+                    </li>
+                    <li>
+                      <p>Optional advanced settings: Provide a separate menu or panel for advanced settings, allowing experienced players to access and fine-tune additional simulation parameters, such as weather, aircraft systems, or realism settings. This way, advanced players can tailor the game to their preferences without cluttering the interface for less experienced players.</p>
+                    </li>
+                  </ul>
+                </li>
+                <li>
+                  <p>Aesthetic Consistency</p>
+                  <p>By keeping the top bar menu always visible and positioned at the top of the screen across different pages, users can easily understand the interface and navigate through various sections of the game. The only exception is when users enter the game mode, and the top bar menu is disabled to provide an uninterrupted gaming experience. This design choice takes into consideration the specific context of the game mode, where the emphasis is on gameplay rather than navigation. Consistency helps create a cohesive and familiar user experience, reducing confusion and cognitive load.</p>
+                </li>
+                <li>
+                  <p>Transfer</p>
+                  <p>We used transfer principle by those familiar interface elements. Use familiar symbols, icons, and terminology from real-world aviation or other flight simulation games in the game's interface. This will help users quickly understand the functions of various interface elements and navigate the game more efficiently.</p>
+                </li>
+                <li>
+                  <p>Control</p>
+                  <p>In our redesigned game, we have improved the control layout to be more intuitive by closely resembling the control schemes found in other flight simulation games. This familiarity allows players to adapt more easily and enhances the overall gaming experience. </p>
+                  <p>It also allows players to customize their control schemes to match their preferences or the settings of other games they've played. For instance, there is an options menu within the game that allows players to modify various game settings. This flexibility will enable players to adapt more easily to your game and create a more personalized experience.</p>
+                </li>
+                <li>
+                  <p>Causality</p>
+                </li>
+                <li>
+                  <p>Fitts’s law </p>
+                  <p>In the context of our game design, we applied Fitts's Law to improve usability by making important buttons larger and centrally positioned. For example:</p>
+                  <ul>
+                    <li>
+                      <p>Start game button: Place the "Start Game" button in a central location on the main menu screen and make it larger than other buttons. This ensures that users can quickly and easily begin the game.</p>
+                    </li>
+                    <li>
+                      <p>Purchase button: In the in-game shop, make the "Purchase" button larger and position it close to the item description or price. This design choice helps users quickly identify and confirm their purchases.</p>
+                    </li>
+                  </ul>
+              
+                </li>
+                <li>
+                  <p>Hick’s law </p>
+                </li>
+                <li>
+                  <p>Feedback </p>
+                  <p>In our game, we implemented feedback in the following ways:</p>
+                  <ul>
+                    <li>
+                      <p>Button interaction feedback: When users press a button in the top menu, provide visual feedback to indicate that the button has been pressed. This is achieved by changing the button's appearance, such as highlighting it. This feedback helps users understand that their input has been registered.</p>
+                    </li>
+                    <li>
+                      <p>Combat feedback: To enhance the gaming experience and provide a sense of accomplishment, implement feedback for successful enemy kills. This can include visual effects, such as explosions or particle effects, and score updates displayed on the screen. Providing this feedback helps players gauge their progress and understand the impact of their actions within the game.</p>
+                    </li>
+                  </ul>
+                  <p>Incorporating these feedback mechanisms will create a more engaging and responsive gaming experience for users, ensuring that they feel connected to the game and its various interactions.</p>
+                </li>
+              </ol>
             </div>
           </Col>
         </Row>
